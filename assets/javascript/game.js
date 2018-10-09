6b9bfe80675fe42c971eb55136dc8512aff7b4b6
Brian Nickila <brian.nickila@gmail.com>
2018-10-09 15:39:29 -0500

    Able to deduct points during battle. I have to figure out how to prevent user from being able to attack many times in a row.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,31 +2,43 @@ $(document).ready(function () {
     //Set your variables...Adjust the values later.
 
     $("#message").html("Please choose your character!");
-//Sets all variables to start the game. Each card has its own hp, attack and counter attack.
-//these will be called when they are in the challenger position and defender position.
-//I have to figure out how to reference the players that were chosen.
+    //Sets all variables to start the game. Each card has its own hp, attack and counter attack.
+    //these will be called when they are in the challenger position and defender position.
+    //I have to figure out how to reference the players that were chosen.
+    //Use objects for players
 
-    var hpA = 100;
-    var attackA = 10;
-    var counterA = 10;
-
-    var hpB = 150;
-    var attackB = 10;
-    var counterB = 10;
-
-    var hpC = 300;
-    var attackC = 10;
-    var counterC = 10;
-
-    var hpD = 500;
-    var attackD = 10;
-    var counterD = 10;
+    var playerA = {
+        hp: 200,
+        attack: 10,
+        attackCon: 10,
+        counter: 10,
+    }
+    var playerB = {
+        hp: 200,
+        attack: 10,
+        counter: 10,
+    }
+    var playerC = {
+        hp: 200,
+        attack: 10,
+        counter: 10,
+    }
+    var playerD = {
+        hp: 200,
+        attack: 10,
+        counter: 10,
+    }
+    console.log(playerB.hp - playerA.attack);
 
     var challenger;
     var defender;
     var playerChosen = false;
     var defenderChosen = false;
-    
+    var wins = 0;
+    var losses = 0;
+
+
+
 
     $("#message").html("Choose your player!");
 
@@ -34,29 +46,30 @@ $(document).ready(function () {
     //After player is chosen, player chooses opponent. Repeat functions but for defender position.
     //These functions will say 'if player was chosen, send to defender position, if not, send to 
     //challenger position. If neither, don't do anything.
-//function myBattle() {
+    //function myBattle() {
     $(".benchCardA").click(function () {
         //the top of each function sends player to DEFENDER spot. This function is for A, next for B and so on.
         if ((playerChosen == true) && (defenderChosen == false)) {
             $(this).hide();
             $(".defenderCard").prepend('<img class="card-img-top" src="assets/images/a.png" alt="Card image cap" value="a">');
-            $(".defenderBody").append('<p class="card-text name">Alpha</p><p class="card-text hp" id="hpA">HP:' + hpA + '</p>');
+            $(".defenderBody").append('<p class="card-text name">Alpha</p><p class="card-text hp">HP:<span id="hpA">' + playerA.hp + '</span></p>');
             defenderChosen = true;
             $("#message").html("Press <b>A</b> to attack!");
             defender = "a";
-            
-        console.log("defender: " + defender);
+
+
+            console.log("defender: " + defender);
         }
         //the bottom of each function sends player to CHALLENGER spot.
         if ((playerChosen == false) && (defenderChosen == false)) {
             $(this).hide();
             $(".challengerCard").prepend('<img class="card-img-top" src="assets/images/a.png" alt="Card image cap" value="a">');
-            $(".challengerBody").append('<p class="card-text name">Alpha</p><p class="card-text hp" id="hpA">HP:' + hpA + '</p>');
+            $(".challengerBody").append('<p class="card-text name">Alpha</p><p class="card-text hp">HP:<span id="hpA">' + playerA.hp + '</span></p>');
             $("#message").html("Choose your opponent!");
             playerChosen = true;
             challenger = "a";
-            console.log("challenger: " + challenger);
-        //this line stops player from doing anything to cards if both above conditions were met.
+            //console.log("challenger: " + challenger);
+            //this line stops player from doing anything to cards if both above conditions were met.
         } else {
             return;
         }
@@ -65,46 +78,46 @@ $(document).ready(function () {
         if ((playerChosen == true) && (defenderChosen == false)) {
             $(this).hide();
             $(".defenderCard").prepend('<img class="card-img-top" src="assets/images/b.png" alt="Card image cap" value="a">');
-            $(".defenderBody").append('<p class="card-text name">Beta</p><p class="card-text" hp id="hpB">HP:' + hpB + '</p>'); 
+            $(".defenderBody").append('<p class="card-text name">Beta</p><p class="card-text hp">HP:<span id="hpB">' + playerB.hp + '</span></p>');
             defenderChosen = true;
             $("#message").html("Press <b>A</b> to attack!");
             defender = "b";
-            console.log("defender: " + defender);
-            
+            //console.log("defender: " + defender);
+
         }
         if ((playerChosen == false) && (defenderChosen == false)) {
             $(this).hide();
             $(".challengerCard").prepend('<img class="card-img-top" src="assets/images/b.png" alt="Card image cap" value="a">');
-            $(".challengerBody").append('<p class="card-text name">Beta</p><p class="card-text" hp id="hpB">HP:' + hpB + '</p>');
+            $(".challengerBody").append('<p class="card-text name">Beta</p><p class="card-tex hp">HP:<span id="hpB">' + playerB.hp + '</span></p>');
             $("#message").html("Choose your opponent!");
             playerChosen = true;
             challenger = "b";
-            console.log("challenger: " + challenger);
+            //console.log("challenger: " + challenger);
 
         } else {
             return;
         }
     });
-    
+
 
     $(".benchCardC").click(function () {
         if ((playerChosen == true) && (defenderChosen == false)) {
             $(this).hide();
             $(".defenderCard").prepend('<img class="card-img-top" src="assets/images/c.png" alt="Card image cap" value="a">');
-            $(".defenderBody").append('<p class="card-text name">Cappa</p><p class="card-text hp" id="hpC">HP:' + hpC + '</p>');   
+            $(".defenderBody").append('<p class="card-text name">Cappa</p><p class="card-text hp" id="hpC">HP:' + playerC.hp + '</p>');
             defenderChosen = true;
             $("#message").html("Press <b>A</b> to attack!");
             defender = "c";
-            console.log("defender: " + defender);
+            //console.log("defender: " + defender);
         }
         if ((playerChosen == false) && (defenderChosen == false)) {
             $(this).hide();
             $(".challengerCard").prepend('<img class="card-img-top" src="assets/images/c.png" alt="Card image cap" value="a">');
-            $(".challengerBody").append('<p class="card-text name">Cappa</p><p class="card-text hp" id="hpC">HP:' + hpC + '</p>');
+            $(".challengerBody").append('<p class="card-text name">Cappa</p><p class="card-text hp" id="hpC">HP:' + playerC.hp + '</p>');
             $("#message").html("Choose your opponent!");
             playerChosen = true;
             challenger = "c";
-            console.log("challenger: " + challenger);
+            //console.log("challenger: " + challenger);
 
         } else {
             return;
@@ -115,55 +128,110 @@ $(document).ready(function () {
         if ((playerChosen == true) && (defenderChosen == false)) {
             $(this).hide();
             $(".defenderCard").prepend('<img class="card-img-top" src="assets/images/d.png" alt="Card image cap" value="a">');
-            $(".defenderBody").append('<p class="card-text name">Delta</p><p class="card-text hp" id="hpA">HP:' + hpD + '</p>');
+            $(".defenderBody").append('<p class="card-text name">Delta</p><p class="card-text hp" id="hpA">HP:' + playerD.hp + '</p>');
             defenderChosen = true;
             $("#message").html("Press <b>A</b> to attack!");
             defender = "d";
-            console.log("defender: " + defender);
+            //console.log("defender: " + defender);
         }
         if ((playerChosen == false) && (defenderChosen == false)) {
             $(this).hide();
             $(".challengerCard").prepend('<img class="card-img-top" src="assets/images/d.png" alt="Card image cap" value="a">');
-            $(".challengerBody").append('<p class="card-text name">Delta</p><p class="card-text hp" id="hpA">HP:' + hpD + '</p>');
+            $(".challengerBody").append('<p class="card-text name">Delta</p><p class="card-text hp" id="hpA">HP:' + playerD.hp + '</p>');
             $("#message").html("Choose your opponent!");
             playerChosen = true;
             challenger = "d";
-            console.log("challenger: " + challenger);
+            //console.log("challenger: " + challenger);
 
         } else {
             return;
         }
-        
     });
-    //console.log("aaa " + defender);
-
-//myBattle();
-
+    //This function calls on the two characters. It says if a challenger and a defender were chosen and the player
+    //hits the A key (capital or lower) it will do what we tell it to do.
     $(document).keyup(function (event) {
-        if ((event.which == 65) && (playerChosen == true) && (defenderChosen == true)){
-        
-        
-        
-        
-        console.log("slow-jam " + challenger);
-        console.log("funky " + defender);
-        } else {
-            console.log("nope")
+        //the letter 'a' is 65 on the keyboard, so this says if the event (or key) is 'a', do this.
+        if ((event.which == 65) && (playerChosen) && (defenderChosen)) {
+            //I'm sure there's a much more concise way to do this with objects but I can't figure it out right now.
+            //This matches challenger to defender and when A is hit, take attack points away from defender, then
+            //take that plus itself every time (first 10, then 20, then 30, etc.) then wait for a fraction of a sec
+            //and do the same to challenger but only the attack points every time.
+
+
+            if ((challenger == "a") &&
+                (defender == "b") &&
+                (playerA.hp > 0) &&
+                (playerB.hp > 0)) {
+                    return;
+                playerB.hp -= (playerA.attack);
+                playerA.attack += playerA.attackCon; //the con version is always worth the original value.
+                $("#hpB").html(playerB.hp);
+                setTimeout(function () {
+                    $("#message").html("Player B fights back!");
+                }, 1000); //waits 1000 miliseconds (1 sec)
+                playerA.hp -= playerB.attack;
+                setTimeout(function () {
+                    $("#hpA").html(playerA.hp);
+                }, 2000);
+            } else {
+                return;
+            }
         }
+        if ((challenger == "a") && (defender == "c")) {
+            score = (player.hp - player.attack)
+        }
+        if ((challenger == "a") && (defender == "d")) {
+            score = (player.hp - player.attack)
+        }
+        if ((challenger == "b") && (defender == "a")) {
+            score = (player.hp - player.attack)
+        }
+        if ((challenger == "b") && (defender == "c")) {
+            score = (player.hp - player.attack)
+        }
+        if ((challenger == "b") && (defender == "d")) {
+            score = (player.hp - player.attack)
+        }
+        if ((challenger == "c") && (defender == "a")) {
+            score = (player.hp - player.attack)
+        }
+        if ((challenger == "c") && (defender == "b")) {
+            score = (player.hp - player.attack)
+        }
+        if ((challenger == "c") && (defender == "d")) {
+            score = (player.hp - player.attack)
+        }
+        if ((challenger == "d") && (defender == "a")) {
+            score = (player.hp - player.attack)
+        }
+        if ((challenger == "d") && (defender == "b")) {
+            score = (player.hp - player.attack)
+        }
+        if ((challenger == "d") && (defender == "c")) {
+            score = (player.hp - player.attack)
+        }
+
+
+
+        //     console.log("slow-jam " + challenger);
+        //     console.log("funky " + defender);
+        //     } else {
+        //         console.log("nope")
+        //     }
+        // });
+
+
+
+
+
+        //     
+        // }
+
     });
 
 
-
-  
-
-    //     
-    // }
 
 });
-
-
-
-
 
 
         //$elect the player from challenger position by saying 'when attack button is pressed, remove 10 from defender's
